037d17cd2d3d9824f2f5cb4836e9b5a6bbc61b71
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-08-25 17:10:47 +0200

    Added types from BE.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import {TestInterface} from "types";
+
 import './App.css';
 
-
-function App() {
-  const foobar: TestInterface = {
-x: 123,
-  };
-
-
-//function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+     <>
+     <header>
+<h1>
+  <strong>Mega</strong> ogłoszenia
+</h1>
+       <button>Dodaj Ogłoszenia</button>
+       <div className="search">
+         <input type="text"/> <button>Szukaj</button>
+       </div>
+     </header>
+       <div className="map"></div>
+     </>
+  )
+};
