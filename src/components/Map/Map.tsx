a8983b0d1d841525755f055b33a472e44c607394
Katarzyna Barska <104527962+Kasia7891@users.noreply.github.com>
2023-08-25 20:34:45 +0200

    Added the map (OpenStreetMaps) and library Leaflet and configuration.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import '../../utils/fix-map-icon';
 
+import 'leaflet/dist/leaflet.css';
 import './map.css';
 export const Map = () => {
     return (
-        <div className="map"></div>
+        <div className="map">
+           < MapContainer center={[51.0645625,19.4430637]} zoom={15} >
+               <TileLayer
+               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+               attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> & contributors"
+               />
+               <Marker position={[51.0645625,19.4430637]}>
+                 <Popup>
+                     <h2>Radomsko</h2>
+                     <p>Centrum Wszechświata</p>
+                 </Popup>
+               </Marker>
+           </MapContainer>
+        </div>
     )
 };
 
